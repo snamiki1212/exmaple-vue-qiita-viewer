59c5e997f90236708f48af94508ba4d79ddf1c3c
snamiki1212 <snamiki1212@gmail.com>
2019-04-20 12:21:09 +0800

    Update Struct

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer">
+    <span>
+      This page is created by shun namiki with Vue(vue-cli).
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    // msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <span>Qiita Viewer with Vue</span>
+    <a href='https://github.com/snamiki1212/qiita-viewer-with-vue'>Github</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    // msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  height: 20px;
+  background-color: red;
+}
+</style>
