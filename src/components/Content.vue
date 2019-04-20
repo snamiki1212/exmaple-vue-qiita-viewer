a66959c9321f7b49274f16f06b2b99deb9c57539
snamiki1212 <snamiki1212@gmail.com>
2019-04-20 14:27:56 +0800

    Update to fetch Qiita articles

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="content">
-    here is content
+    <p>here is content</p>
+    <p>{{ getArticles() }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Content',
-  props: {
-    // msg: String
+  data() {
+    return {
+      articles: [],
+    }
+  },
+  methods: {
+    getArticles: function(){
+      const url = 'https://qiita.com/api/v2/items'
+      fetch(url)
+        .then(r => r.json())
+        // .then(json => {console.log('json',json); console.log('articles', this.articles); [].push.apply(this.articles, json)});
+        .then(json => [].push.apply(this.articles, json));
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
